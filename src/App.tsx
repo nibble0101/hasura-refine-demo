b98c862c8287f5f0ae189cfe070f9c2ee9dc9477
Joseph Mawa <mj.mawa@yahoo.com>
2023-12-15 12:25:18 +0300

    Initial commit from create-refine-app

---
+++ src/App.tsx
@@ -0,0 +1,83 @@
+import { GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
+import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
+import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+
+import { useNotificationProvider } from "@refinedev/antd";
+import "@refinedev/antd/dist/reset.css";
+
+import dataProvider, {
+  GraphQLClient,
+  graphqlWS,
+  liveProvider,
+} from "@refinedev/hasura";
+import routerBindings, {
+  DocumentTitleHandler,
+  UnsavedChangesNotifier,
+} from "@refinedev/react-router-v6";
+import { App as AntdApp } from "antd";
+import { useTranslation } from "react-i18next";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { authProvider } from "./authProvider";
+import { ColorModeContextProvider } from "./contexts/color-mode";
+
+const API_URL = "https://flowing-mammal-24.hasura.app/v1/graphql";
+const WS_URL = "ws://flowing-mammal-24.hasura.app/v1/graphql";
+
+const client = new GraphQLClient(API_URL, {
+  headers: {
+    "x-hasura-role": "public",
+  },
+});
+
+const webSocketClient = graphqlWS.createClient({
+  url: WS_URL,
+});
+
+function App() {
+  const { t, i18n } = useTranslation();
+
+  const i18nProvider = {
+    translate: (key: string, params: object) => t(key, params),
+    changeLocale: (lang: string) => i18n.changeLanguage(lang),
+    getLocale: () => i18n.language,
+  };
+
+  return (
+    <BrowserRouter>
+      <GitHubBanner />
+      <RefineKbarProvider>
+        <ColorModeContextProvider>
+          <AntdApp>
+            <DevtoolsProvider>
+              <Refine
+                dataProvider={dataProvider(client)}
+                liveProvider={liveProvider(webSocketClient)}
+                notificationProvider={useNotificationProvider}
+                routerProvider={routerBindings}
+                authProvider={authProvider}
+                i18nProvider={i18nProvider}
+                options={{
+                  syncWithLocation: true,
+                  warnWhenUnsavedChanges: true,
+                  useNewQueryKeys: true,
+                  projectId: "kD4sL9-SETcry-ni4EBI",
+                  liveMode: "auto",
+                }}
+              >
+                <Routes>
+                  <Route index element={<WelcomePage />} />
+                </Routes>
+                <RefineKbar />
+                <UnsavedChangesNotifier />
+                <DocumentTitleHandler />
+              </Refine>
+              <DevtoolsPanel />
+            </DevtoolsProvider>
+          </AntdApp>
+        </ColorModeContextProvider>
+      </RefineKbarProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
